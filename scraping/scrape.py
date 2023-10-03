944ec2d360dd3fe7ec7c3bb68f9f869b57d7c330
Makenakalei <robisonmakena@gmail.com>
2023-10-03 16:40:38 -0700

    finished db

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -6,21 +6,16 @@ url = "https://en.wikipedia.org/wiki/Wes_Anderson_filmography"
 browser = mechanicalsoup.StatefulBrowser()
 browser.open(url)
 
-th = browser.page.find_all("th")
-year = [value.text.replace("\n", "") for value in th]
-year = year[:5]
-print("year" )
-print(year)
 
 td = browser.page.find_all("td")
 columns = [value.text.replace("\n", "") for value in td]
 # print(columns)
 
 
-columns = columns[0:54]
+columns = columns[0:55]
 print(columns)
 
-column_names = [
+column_names = [ "Year",
                 "Title",
                 "Director",
                 "Writer",
@@ -30,10 +25,10 @@ column_names = [
 # column[1:][::11]
 # column[2:][::11]
 
-dictionary = {"Year": year}
-
+# dictionary = {"Year": year}
+dictionary = {}
 for idx, key in enumerate(column_names):
-    dictionary[key] = columns[idx:][::11]
+    dictionary[key] = columns[idx:][::10]
 print(len(dictionary))
 print(len(dictionary['Year']))
 print(len(dictionary['Title']))
@@ -46,7 +41,7 @@ print(df.tail())
 connection = sqlite3.connect("wesanderson.db")
 cursor = connection.cursor()
 cursor.execute("DROP TABLE IF EXISTS wes_anderson_movies")
-cursor.execute("create table wes_anderson_movies (Year, " + ",".join(column_names) + ")")
+cursor.execute("create table wes_anderson_movies (Year, " + ",".join(column_names[1::]) + ")")
 for i in range(len(df)):
     cursor.execute("insert into wes_anderson_movies values (?,?,?,?,?)", df.iloc[i])
 
